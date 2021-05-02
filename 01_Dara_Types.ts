03bc47b355c79d0758c4e60c1e1b0bc8938633bf
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-02 12:17:56 +0300

    feat: some minor changes

--- 01_Dara_Types.ts
+++ 01_Dara_Types.ts
@@ -1,21 +1,32 @@
-// for file compilation call the following string in Terminal: tsc 01_Dara_Types.ts
+﻿// for file compilation call the following string in Terminal: tsc 01_Dara_Types.ts
 // You can not reassign another data type!!!
 // String
+
 const str: string = "Hello";
+
 // Boolean
+
 const isFetching: boolean = true;
 const isLoading: boolean = false;
+
 // number
+
 let int: number = 42;
 let float: number = 23.5;
 let number: number = 3e10;
+
 // Arrays of number
+
 const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13];
 const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13];
+
 // Arrsay of strings
+
 const stringArray: string[] = ["a", "b", "c"];
 const stringArray2: Array<string> = ["a", "b", "c"];
+
 // Tuple (кортеж)
+
 const contact: [string, number] = ["valdilen", 1234567];
 // if you need to reassignd data type use ANY data type
 let variable: any = 42;
@@ -27,7 +38,9 @@ function sayMyName(name: string): void {
   console.log(name);
 }
 sayMyName("HeizenBerg");
+
 // NEVER data type
+
 function throwError(meassage: string) {
   for (let i = 0; i < 10; i++) {
     throw new Error();
